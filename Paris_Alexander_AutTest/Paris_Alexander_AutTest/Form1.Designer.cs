d373878cd51f37f7819c64efae4335523cd1e947
Schularbeit10 <Schularbeit10@rg-me.schule.suedtirol.it>
2018-03-06 12:40:02 +0100

    infobox&tanken

--- Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.Designer.cs
+++ Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.Designer.cs
@@ -28,12 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtb_fahern = new System.Windows.Forms.TextBox();
+            this.txtb_tanken = new System.Windows.Forms.TextBox();
+            this.txtb_verbrauch = new System.Windows.Forms.TextBox();
+            this.txtb_tankgröße = new System.Windows.Forms.TextBox();
+            this.txtb_ps = new System.Windows.Forms.TextBox();
+            this.txtb_marke = new System.Windows.Forms.TextBox();
+            this.prob_tankstand = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_autoerstellen = new System.Windows.Forms.Button();
+            this.btn_tanken = new System.Windows.Forms.Button();
+            this.btn_fahren = new System.Windows.Forms.Button();
+            this.comb_farbe = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comb_farbe);
+            this.groupBox1.Controls.Add(this.btn_autoerstellen);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtb_marke);
+            this.groupBox1.Controls.Add(this.txtb_ps);
+            this.groupBox1.Controls.Add(this.txtb_tankgröße);
+            this.groupBox1.Controls.Add(this.txtb_verbrauch);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(451, 205);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(388, 334);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(246, 187);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_tanken);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.prob_tankstand);
+            this.groupBox3.Controls.Add(this.txtb_tanken);
+            this.groupBox3.Location = new System.Drawing.Point(12, 269);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(344, 143);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btn_fahren);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.txtb_fahern);
+            this.groupBox4.Location = new System.Drawing.Point(12, 450);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(344, 71);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(71, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // txtb_fahern
+            // 
+            this.txtb_fahern.Location = new System.Drawing.Point(27, 31);
+            this.txtb_fahern.Name = "txtb_fahern";
+            this.txtb_fahern.Size = new System.Drawing.Size(100, 20);
+            this.txtb_fahern.TabIndex = 0;
+            // 
+            // txtb_tanken
+            // 
+            this.txtb_tanken.Location = new System.Drawing.Point(27, 53);
+            this.txtb_tanken.Name = "txtb_tanken";
+            this.txtb_tanken.Size = new System.Drawing.Size(100, 20);
+            this.txtb_tanken.TabIndex = 1;
+            // 
+            // txtb_verbrauch
+            // 
+            this.txtb_verbrauch.Location = new System.Drawing.Point(261, 97);
+            this.txtb_verbrauch.Name = "txtb_verbrauch";
+            this.txtb_verbrauch.Size = new System.Drawing.Size(100, 20);
+            this.txtb_verbrauch.TabIndex = 1;
+            // 
+            // txtb_tankgröße
+            // 
+            this.txtb_tankgröße.Location = new System.Drawing.Point(261, 36);
+            this.txtb_tankgröße.Name = "txtb_tankgröße";
+            this.txtb_tankgröße.Size = new System.Drawing.Size(100, 20);
+            this.txtb_tankgröße.TabIndex = 2;
+            // 
+            // txtb_ps
+            // 
+            this.txtb_ps.Location = new System.Drawing.Point(44, 139);
+            this.txtb_ps.Name = "txtb_ps";
+            this.txtb_ps.Size = new System.Drawing.Size(100, 20);
+            this.txtb_ps.TabIndex = 3;
+            // 
+            // txtb_marke
+            // 
+            this.txtb_marke.Location = new System.Drawing.Point(44, 36);
+            this.txtb_marke.Name = "txtb_marke";
+            this.txtb_marke.Size = new System.Drawing.Size(100, 20);
+            this.txtb_marke.TabIndex = 4;
+            // 
+            // prob_tankstand
+            // 
+            this.prob_tankstand.Location = new System.Drawing.Point(27, 89);
+            this.prob_tankstand.Name = "prob_tankstand";
+            this.prob_tankstand.Size = new System.Drawing.Size(260, 48);
+            this.prob_tankstand.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // btn_autoerstellen
+            // 
+            this.btn_autoerstellen.Location = new System.Drawing.Point(269, 155);
+            this.btn_autoerstellen.Name = "btn_autoerstellen";
+            this.btn_autoerstellen.Size = new System.Drawing.Size(75, 23);
+            this.btn_autoerstellen.TabIndex = 10;
+            this.btn_autoerstellen.Text = "button1";
+            this.btn_autoerstellen.UseVisualStyleBackColor = true;
+            // 
+            // btn_tanken
+            // 
+            this.btn_tanken.Location = new System.Drawing.Point(244, 49);
+            this.btn_tanken.Name = "btn_tanken";
+            this.btn_tanken.Size = new System.Drawing.Size(75, 23);
+            this.btn_tanken.TabIndex = 11;
+            this.btn_tanken.Text = "button2";
+            this.btn_tanken.UseVisualStyleBackColor = true;
+            // 
+            // btn_fahren
+            // 
+            this.btn_fahren.Location = new System.Drawing.Point(221, 27);
+            this.btn_fahren.Name = "btn_fahren";
+            this.btn_fahren.Size = new System.Drawing.Size(75, 23);
+            this.btn_fahren.TabIndex = 11;
+            this.btn_fahren.Text = "button3";
+            this.btn_fahren.UseVisualStyleBackColor = true;
+            // 
+            // comb_farbe
+            // 
+            this.comb_farbe.FormattingEnabled = true;
+            this.comb_farbe.Items.AddRange(new object[] {
+            "weiß"});
+            this.comb_farbe.Location = new System.Drawing.Point(63, 88);
+            this.comb_farbe.Name = "comb_farbe";
+            this.comb_farbe.Size = new System.Drawing.Size(121, 21);
+            this.comb_farbe.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 533);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comb_farbe;
+        private System.Windows.Forms.Button btn_autoerstellen;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtb_marke;
+        private System.Windows.Forms.TextBox txtb_ps;
+        private System.Windows.Forms.TextBox txtb_tankgröße;
+        private System.Windows.Forms.TextBox txtb_verbrauch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_tanken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar prob_tankstand;
+        private System.Windows.Forms.TextBox txtb_tanken;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btn_fahren;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtb_fahern;
     }
 }
 
--- Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.cs
+++ Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.cs
@@ -15,5 +15,18 @@ namespace Paris_Alexander_AutTest
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            groupBox2.Enabled = false;
+            groupBox3.Enabled = false;
+            groupBox4.Enabled = false;
+            if (txtb_marke.Text != "" && comb_farbe.Text != "" && txtb_ps.Text != "" && txtb_tankgröße.Text != "" && txtb_verbrauch.Text != "")
+            {
+                groupBox2.Enabled = true;
+                groupBox3.Enabled = true;
+                groupBox4.Enabled = true;
+            }
+        }
     }
 }
