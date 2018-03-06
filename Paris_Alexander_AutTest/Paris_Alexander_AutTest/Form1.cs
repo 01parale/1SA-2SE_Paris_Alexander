c347a5209e83e66f8a67974e6c4ec7feae1d4d73
Schularbeit10 <Schularbeit10@rg-me.schule.suedtirol.it>
2018-03-06 12:32:18 +0100

    weiter

---
+++ Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Paris_Alexander_AutTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
