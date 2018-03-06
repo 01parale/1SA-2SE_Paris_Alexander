a8b1211aab80f5c1d4ecf98c6dc310be093c66db
Schularbeit10 <Schularbeit10@rg-me.schule.suedtirol.it>
2018-03-06 12:58:48 +0100

    erweiterung

--- Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.cs
+++ Paris_Alexander_AutTest/Paris_Alexander_AutTest/Form1.cs
@@ -15,18 +15,31 @@ namespace Paris_Alexander_AutTest
         {
             InitializeComponent();
         }
-
+        public string Tankstand;
         private void groupBox1_Enter(object sender, EventArgs e)
         {
             groupBox2.Enabled = false;
             groupBox3.Enabled = false;
             groupBox4.Enabled = false;
+           
+        }
+
+        private void btn_autoerstellen_Click(object sender, EventArgs e)
+        {
             if (txtb_marke.Text != "" && comb_farbe.Text != "" && txtb_ps.Text != "" && txtb_tankgröße.Text != "" && txtb_verbrauch.Text != "")
             {
                 groupBox2.Enabled = true;
                 groupBox3.Enabled = true;
                 groupBox4.Enabled = true;
+                rtxtb_info.Text += "Marke:" + txtb_marke.Text;
+                rtxtb_info.Text += "    Farbe:" + comb_farbe.Text;
             }
+        }
+
+        private void btn_tanken_Click(object sender, EventArgs e)
+        {
+             Tankstand += txtb_tanken.Text;
+             rtxtb_info.Text += "   Tankstand:" + Tankstand+ "l";
         }
     }
 }
